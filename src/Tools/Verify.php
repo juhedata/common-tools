a3423198a25f75acef2e643ba5310ba25f8a4203
xwzhang <zhangxw@think-land.com>
2019-06-14 14:41:02 +0800

    邮箱格式校验修正

--- src/Tools/Verify.php
+++ src/Tools/Verify.php
@@ -214,7 +214,7 @@ class Verify
      */
     public static function isEmail($email)
     {
-        $patrn = '/^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.|\-]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/';
+        $patrn = '/^[\da-z]+([-._]?[\da-z]+)*@[\da-z]+([-._]?[\da-z]+)*(\.[a-zA-Z]{2,3})+$/i';
         if (preg_match($patrn, $email) && strlen($email) < 64) {
             if (self::filterTempMail($email)) {
                 return true;
